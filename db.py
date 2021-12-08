ca00514c5df80cd07e3c34aa3cea910828412e9b
carolinesadlerr <60328236+carolinesadlerr@users.noreply.github.com>
2021-12-08 15:01:01 -0500

    Initial commit

---
+++ db.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+
+class Database:
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS item (id INTEGER PRIMARY KEY, name text, description text, "
+                         "price text) ")
+        self.conn.commit()
+
+    def fetch(self):
+        self.cur.execute("SELECT * FROM item")
+        rows = self.cur.fetchall()
+        return rows
+
+    def insert(self, name, description, price):
+        self.cur.execute("INSERT INTO item VALUES(NULL,?,?,?)", (name, description, price))
+        self.conn.commit()
+
+    def remove(self, id):
+        self.cur.execute("DELETE FROM item WHERE id=?", (id,))
+        self.conn.commit()
+
+    def update(self, id, name, description, price):
+        self.cur.execute("UPDATE item SET name=?, description=?,price=? WHERE id = ?",
+                         (name, description, price, id))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
+
+
+db = Database('thriftstore.db')
+db.insert("Shirt", "size small blue shirt", "5")
+db.insert("Pants", "size 14 straight cut jeans", "10")
+db.insert("T-shirt", "large band t", "7")
---
+++ main.py
@@ -0,0 +1,117 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+from db import Database
+
+db = Database('thriftstore.db')
+
+
+def populate_list():
+    item_list.delete(0, END)
+    for row in db.fetch():
+        item_list.insert(END, row)
+
+
+def add_item():
+    if item_text.get() == '' or des_text.get() == '' or price_text.get() == '':
+        messagebox.showerror('Required Fields', 'Please include all fields')
+        return
+    db.insert(item_text.get(), des_text.get(), price_text.get())
+    item_list.delete(0, END)
+    item_list.insert(END, (item_text.get(), des_text.get(), price_text.get()))
+    populate_list()
+
+
+# Bind select to an event
+def select_item(event):
+    try:
+        global selected_item
+        index = item_list.curselection()[0]
+        selected_item = item_list.get(index)
+
+        item_entry.delete(0, END)
+        item_entry.insert(END, selected_item[1])
+        des_entry.delete(0, END)
+        des_entry.insert(END, selected_item[2])
+        price_entry.delete(0, END)
+        price_entry.insert(END, selected_item[3])
+    except IndexError:
+        pass
+
+
+def remove_item():
+    db.remove(selected_item[0])
+    clear_text()
+    populate_list()
+
+
+def update_item():
+    db.update(selected_item[0], item_text.get(), des_text.get(), price_text.get())
+    populate_list()
+
+
+def clear_text():
+    item_entry.delete(0, END)
+    des_entry.delete(0, END)
+    price_entry.delete(0, END)
+
+
+# create window
+app = Tk()
+
+# Item
+item_text = StringVar()
+item_label = Label(app, text='Item', font=('bold', 14), pady=20)
+item_label.grid(row=0, column=0)
+item_entry = Entry(app, textvariable=item_text)
+item_entry.grid(row=0, column=1)
+
+# Description
+des_text = StringVar()
+des_label = Label(app, text='Description', font=('bold', 14))
+des_label.grid(row=0, column=2)
+des_entry = Entry(app, textvariable=des_text)
+des_entry.grid(row=0, column=3)
+
+# Price
+price_text = StringVar()
+price_label = Label(app, text='Price', font=('bold', 14))
+price_label.grid(row=1, column=0)
+price_entry = Entry(app, textvariable=price_text)
+price_entry.grid(row=1, column=1)
+
+# Info List Box
+item_list = Listbox(app, height=8, width=50)
+item_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
+
+# create scrollbar
+scrollbar = Scrollbar(app)
+scrollbar.grid(row=3, column=3)
+
+# set scrollbar to listbox
+item_list.configure(yscrollcommand=scrollbar.set)
+scrollbar.configure(command=item_list.yview)
+
+# Bind select
+item_list.bind('<<ListboxSelect>>', select_item)
+
+# Buttons
+add_btn = Button(app, text='Add Item', width=12, command=add_item)
+add_btn.grid(row=2, column=0, pady=20)
+
+remove_btn = Button(app, text='Remove Item', width=12, command=remove_item)
+remove_btn.grid(row=2, column=1)
+
+update_btn = Button(app, text='Update Item', width=12, command=update_item)
+update_btn.grid(row=2, column=2)
+
+clear_btn = Button(app, text='Clear Input', width=12, command=clear_text)
+clear_btn.grid(row=2, column=3)
+
+app.title('Thrift Store')
+app.geometry('700x350')
+
+# populate data
+populate_list()
+# start program
+app.mainloop()
